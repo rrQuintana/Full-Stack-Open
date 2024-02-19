efd449c9b460ed28b9bbef7cd2e78d7732941485
Roberto Quintana <rrquintana7@gmail.com>
2024-02-18 19:43:18 -0600

    part6: anecdote form validation notification

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,13 +1,24 @@
 import { useMutation, useQueryClient } from 'react-query'
 import { createAnecdote, voteAnecdote } from '../requests'
+import { useContext } from 'react'
+import NotificationContext from '../notificationContext'
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  const [notification, dispatchNotification] = useContext(NotificationContext)
 
   const newAnecdoteMutation = useMutation(createAnecdote, {
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData('anecdotes')
       queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+    },
+    onError: (error) => {
+      if (error.response.status === 400) {
+        dispatchNotification({ type: 'SET_NOTIFICATION', data: `Too short anecdote, must have length 5 or more` })
+        setTimeout(() => {
+          dispatchNotification({ type: 'CLEAR_NOTIFICATION' })
+        }, 3000)
+      }
     }
   })
 
--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -4,7 +4,7 @@ export const getAnecdotes = () =>
   axios.get('http://localhost:3001/anecdotes').then(res => res.data)
 
 export const createAnecdote = (content) =>
-  axios.post('http://localhost:3001/anecdotes', { content, votes: 0 }).then(res => res.data).catch(err => console.log(err))
+  axios.post('http://localhost:3001/anecdotes', { content, votes: 0 }).then(res => res.data)
 
 export const voteAnecdote = (anecdote) =>
   axios.put(`http://localhost:3001/anecdotes/${anecdote.id}`, { ...anecdote, votes: anecdote.votes + 1 }).then(res => res.data).catch(err => console.log(err))
